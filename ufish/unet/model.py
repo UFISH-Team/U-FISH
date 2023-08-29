a2dcf6b3aaaa45a1c497d0828d9b28891fcf877d
Nanguage <Nanguage@yahoo.com>
2023-08-29 23:44:32 +0800

    fix channel unmatch bug

--- ufish/unet/model.py
+++ ufish/unet/model.py
@@ -6,14 +6,17 @@ import torch.nn.functional as F
 class ResidualBlock(nn.Module):
     def __init__(self, in_channels, out_channels):
         super(ResidualBlock, self).__init__()
-        self.conv1 = nn.Conv2d(
+        self.conv0 = nn.Conv2d(
             in_channels, out_channels, kernel_size=1, padding=0)
+        self.conv1 = nn.Conv2d(
+            out_channels, out_channels, kernel_size=1, padding=0)
         self.conv2 = nn.Conv2d(
             out_channels, out_channels, kernel_size=3, padding=1)
         self.BN1 = nn.BatchNorm2d(out_channels)
         self.BN2 = nn.BatchNorm2d(out_channels)
 
     def forward(self, x):
+        x = self.conv0(x)
         out = self.conv1(x)
         out = self.BN1(out)
         out = F.relu(out, inplace=True)
